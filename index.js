b5a55b60067175bdb86f610a43ce3f5f1f16d05d
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-10-19 19:14:14 -0400

    working finally!

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-//modules import
+// Modules import
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
 const { triangle, circle, square } = require('./lib/shapes');
 
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-    type: 'list',
-    name: 'ChooseShape',
-    message: 'choose shape',
-    choices: ['triangle', 'circle', 'square']
+      type: 'list',
+      name: 'ChooseShape',
+      message: 'Choose a shape',
+      choices: ['triangle', 'circle', 'square'],
     },
     {
-        type: 'list',
-        name: 'ChooseColor',
-        message: 'choose color',
-        choices: ['pink', 'blue', 'green']
+      type: 'list',
+      name: 'ChooseColor',
+      message: 'Choose a color',
+      choices: ['pink', 'blue', 'green'],
     },
     {
-        type: 'input',
-        name: 'lettersForLogo',
-        message: 'input 1-3 letters',
-        validate: (input)=>{
-            if(input.length >= 1 && input.length <=3){
-                return true; 
-            } else{
-                return 'up to 3 letters only, please!'
-            }
-        },
-    }
-])
-
-inquirer.prompt(questions).then((answers) => {
+      type: 'input',
+      name: 'lettersForLogo',
+      message: 'Input 1-3 letters',
+      validate: (input) => {
+        if (input.length >= 1 && input.length <= 3) {
+          return true;
+        } else {
+          return 'Up to 3 letters only, please!';
+        }
+      },
+    },
+  ])
+  .then((answers) => {
     const { lettersForLogo, ChooseColor, ChooseShape } = answers;
-    const shape = ChooseShape;
     let svg = '';
-    
-    shape.setColor(ChooseColor);
-    
-    if (shape === 'triangle') {
-        svg = triangle(lettersForLogo);
-    } else if (shape === 'circle') {
-        svg = circle(lettersForLogo);
-    } else if (shape === 'square') {
-        svg = square(lettersForLogo);
-    }   
-    fs.writeFile('logo.svg', svg, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    })
 
-});
+    if (ChooseShape === 'triangle') {
+      svg = triangle(lettersForLogo, ChooseColor);
+    } else if (ChooseShape === 'circle') {
+      svg = circle(lettersForLogo, ChooseColor);
+    } else if (ChooseShape === 'square') {
+      svg = square(lettersForLogo, ChooseColor);
+    }
+
+    fs.writeFile('logo.svg', svg, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  });
